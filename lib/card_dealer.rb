f322ffb1c1ab50570f997bb5d54e0664ac953289
Alejandro <Alejandro@Algy>
2015-11-11 11:00:02 +0100

    Modificado el card_dealer Añadidos las cartas de monstruos al init de card_dealer y las cartas de tesoros.

--- lib/card_dealer.rb
+++ lib/card_dealer.rb
@@ -7,10 +7,13 @@ include Singleton
 class CardDealer
   @@instance = nil
   def initialize
-    
+    @used_treasures = Array.new
+    @unused_treasures = Array.new
+    @used_monsters = Array.new
+    @unused_monsters = Array.new
   end
   
-  attr_reader :instance
+  attr_reader :instance 
   attr_accessor :used_treasures, :unused_treasures , :used_monsters , :unused_monsters
   
   def get_instance
@@ -21,6 +24,39 @@ class CardDealer
   end
   
   def init_treasure_card_deck
+        
+        unused_treasures.add(new Treasure("¡Si mi amo!", 4, TreasureKind::HELMET));
+        unused_treasures.add(new Treasure("Botas de investigacion", 3, TreasureKind::SHOES));
+        unused_treasures.add(new Treasure("Capucha de Cthutlu", 3, TreasureKind::HELMET));
+        unused_treasures.add(new Treasure("A prueba de babas", 2, TreasureKind::ARMOR));
+        unused_treasures.add(new Treasure("Botas de lluvia acida", 1, TreasureKind::BOTHHANDS));
+        unused_treasures.add(new Treasure("Casco minero", 2, TreasureKind::HELMET));
+        unused_treasures.add(new Treasure("Ametralladora Thompson", 4, TreasureKind::BOTHHANDS));
+        unused_treasures.add(new Treasure("Camiseta de la UGR", 1, TreasureKind::ARMOR));
+        unused_treasures.add(new Treasure("Clavo de rail ferroviario", 3, TreasureKind::ONEHAND));
+        unused_treasures.add(new Treasure("Cuchillo de sushi arcano", 2, TreasureKind::ONEHAND));
+        unused_treasures.add(new Treasure("Fez alopodo", 3, TreasureKind::HELMET));
+        unused_treasures.add(new Treasure("Hacha prehistorica", 2, TreasureKind::ONEHAND));
+        unused_treasures.add(new Treasure("El aparato del Pr.Tesla", 4, TreasureKind::ARMOR));
+        unused_treasures.add(new Treasure("Gaita", 4, TreasureKind::BOTHHANDS));
+        unused_treasures.add(new Treasure("Insecticida", 2, TreasureKind::ONEHAND));
+        unused_treasures.add(new Treasure("Escopeta de 3 cañones", 4, TreasureKind::BOTHHANDS));
+        unused_treasures.add(new Treasure("Garabato mistico", 2, TreasureKind::ONEHAND));
+        unused_treasures.add(new Treasure("La rebeca metalica", 2, TreasureKind::ARMOR));
+        unused_treasures.add(new Treasure("Lanzallamas", 4, TreasureKind::BOTHHANDS));
+        unused_treasures.add(new Treasure("Necrocomicon", 1, TreasureKind::ONEHAND));
+        unused_treasures.add(new Treasure("Necronomicon", 5, TreasureKind::BOTHHANDS));
+        unused_treasures.add(new Treasure("Linterna a 2 manos", 3, TreasureKind::BOTHHANDS));
+        unused_treasures.add(new Treasure("Necrognomicon", 2, TreasureKind::ONEHAND));
+        unused_treasures.add(new Treasure("Necrotelecom", 2, TreasureKind::HELMET));
+        unused_treasures.add(new Treasure("Mazo de los antiguos", 3, TreasureKind::ONEHAND));
+        unused_treasures.add(new Treasure("Necroplayboycon", 3, TreasureKind::ONEHAND));
+        unused_treasures.add(new Treasure("Porra preternatural", 2, TreasureKind::ONEHAND));
+        unused_treasures.add(new Treasure("Shogulador", 1, TreasureKind::BOTHHANDS));
+        unused_treasures.add(new Treasure("Varita de atizamiento", 3, TreasureKind::ONEHAND));
+        unused_treasures.add(new Treasure("Tentaculo de pega", 2, TreasureKind::HELMET));
+        unused_treasures.add(new Treasure("Zapato deja-amigos", 1, TreasureKind::SHOES));
+     
   end
   
   def init_monster_card_deck
